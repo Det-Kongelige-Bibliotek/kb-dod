9e8a33fa8c8b1908d1e2563c199ad1fb42f6c5b6
Nader Khatibi <nkh@kb.dk>
2020-04-24 11:54:26 +0200

    Before move

--- src/main/java/dk/kb/dod/service/DodRest.java
+++ src/main/java/dk/kb/dod/service/DodRest.java
@@ -1,8 +1,12 @@
 package dk.kb.dod.service;
 
+import dk.kb.alma.gen.Item;
+import dk.kb.dod.AlmaClient;
 import dk.kb.dod.facade.DodFacade;
 
 import javax.ws.rs.*;
+import java.io.IOException;
+import java.util.Properties;
 
 @Path("/services")
 public class DodRest {
@@ -17,7 +21,18 @@ public class DodRest {
 
     @GET
     @Path("dod/{barcode}")
-    public String dodWork(@PathParam("barcode") String barcode) {
+    public String dodWork(@PathParam("barcode") String barcode) throws IOException {
+        Properties dodpro = new Properties();
+        dodpro.load(DodRest.class.getClassLoader().getResourceAsStream("dod.properties"));
+
+        String apikey = dodpro.getProperty("alma.apikey");
+        String url = dodpro.getProperty("alma.url");
+        AlmaClient almaclient = new AlmaClient(url, apikey,10000,30000,"da");
+        Item myItem = almaclient.getItem(barcode);
+
+        // createRecord(myItem.getBibData().getMmsId(), getUrl(barcode);
+
+
         return facade.dodWork(barcode);
     }
 }
